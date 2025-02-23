e9a98c9b5af4b921ca5d94f90b9facd9ac340897
ilhamalamsyh <ilhamalamsyah510@gmail.com>
2025-02-23 07:47:55 +0700

    chore: adjust the apollo client handle authLink

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,6 +1,13 @@
 // src/graphql/apollo-client.ts
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  from,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const apiUrl: string | undefined = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 if (!apiUrl) {
   throw new Error(
@@ -16,8 +23,20 @@ const httpLink = createHttpLink({
   },
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token =
+    typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 // Initialize the Apollo Client
 export const client = new ApolloClient({
-  link: httpLink,
+  link: from([authLink, httpLink]),
   cache: new InMemoryCache(), // Cache configuration for client-side caching
 });
