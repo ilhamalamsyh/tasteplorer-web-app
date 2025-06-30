d153e86b1210c6309e42c948e7b956985239ee8d
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2025-06-30 15:19:49 +0700

    feat: adding initial home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-// src/app/index.tsx
-'use-client';
-
+'use client';
 import React from 'react';
-import '../styles/tailwind.css';
 
 const Home = () => {
   return (
-    <main className="flex-grow">
-      <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold text-center mb-6">
-          Recipe List Page
-        </h1>
-      </div>
+    <main className="flex-grow bg-white min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-gray-400">
+        Welcome! Please visit /explorer for the main experience.
+      </h1>
     </main>
   );
 };
