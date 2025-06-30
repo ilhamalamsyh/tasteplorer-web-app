bd408bd4ed86edc3eb78aafe6557413b96e64195
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2025-07-01 06:53:01 +0700

    chore: adding dummy data for a while

---
+++ src/core/data/feedPosts.ts
@@ -0,0 +1,129 @@
+export const feedPosts = [
+  {
+    id: 1,
+    user: {
+      name: 'Adythia R',
+      avatar: 'https://randomuser.me/api/portraits/men/11.jpg',
+    },
+    community: {
+      name: 'Lets do Brunch',
+      avatar:
+        'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=40&q=80',
+    },
+    time: '10h',
+    text: 'Definitely different. A great flavorful vegetarian meal though. Ill try it again sometime. Easy to make.',
+    image:
+      'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=600&q=80',
+    source: {
+      title: 'Mediterranean Baked Sweet Potatoes',
+      url: 'https://minimalistbaker.com',
+      image:
+        'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=40&q=80',
+    },
+    liked: false,
+    bookmarked: false,
+    comments: 3,
+  },
+  {
+    id: 2,
+    user: {
+      name: 'Beri',
+      avatar: 'https://randomuser.me/api/portraits/women/12.jpg',
+    },
+    community: {
+      name: 'Everyday Healthy',
+      avatar:
+        'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=40&q=80',
+    },
+    time: '2h',
+    text: 'Quick, easy, and healthy lunch idea!',
+    image:
+      'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=600&q=80',
+    source: {
+      title: 'Healthy Avocado Toast',
+      url: 'https://avocadotoast.com',
+      image:
+        'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=40&q=80',
+    },
+    liked: true,
+    bookmarked: true,
+    comments: 5,
+  },
+  {
+    id: 3,
+    user: {
+      name: 'Beri',
+      avatar: 'https://randomuser.me/api/portraits/women/12.jpg',
+    },
+    community: {
+      name: 'Everyday Healthy',
+      avatar:
+        'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=40&q=80',
+    },
+    time: '2h',
+    text: 'Quick, easy, and healthy lunch idea!',
+    image:
+      'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=600&q=80',
+    source: {
+      title: 'Healthy Avocado Toast',
+      url: 'https://avocadotoast.com',
+      image:
+        'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=40&q=80',
+    },
+    liked: true,
+    bookmarked: true,
+    comments: 5,
+  },
+
+  {
+    id: 4,
+    user: {
+      name: 'Beri',
+      avatar: 'https://randomuser.me/api/portraits/women/12.jpg',
+    },
+    community: {
+      name: 'Everyday Healthy',
+      avatar:
+        'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=40&q=80',
+    },
+    time: '2h',
+    text: 'Quick, easy, and healthy lunch idea!',
+    image:
+      'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=600&q=80',
+    source: {
+      title: 'Healthy Avocado Toast',
+      url: 'https://avocadotoast.com',
+      image:
+        'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=40&q=80',
+    },
+    liked: true,
+    bookmarked: true,
+    comments: 5,
+  },
+
+  {
+    id: 5,
+    user: {
+      name: 'Beri',
+      avatar: 'https://randomuser.me/api/portraits/women/12.jpg',
+    },
+    community: {
+      name: 'Everyday Healthy',
+      avatar:
+        'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=40&q=80',
+    },
+    time: '2h',
+    text: 'Quick, easy, and healthy lunch idea!',
+    image:
+      'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=600&q=80',
+    source: {
+      title: 'Healthy Avocado Toast',
+      url: 'https://avocadotoast.com',
+      image:
+        'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=40&q=80',
+    },
+    liked: true,
+    bookmarked: true,
+    comments: 5,
+  },
+];
---
+++ src/core/data/profile.ts
@@ -0,0 +1,6 @@
+export const profile = {
+  name: 'Mikasa',
+  avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+  following: 3,
+  followers: 0,
+};
